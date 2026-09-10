d0120c5febaa0c307236a37ad615139a3bdcca01
Maely Peixoto Santos <maely.santos@edu.senai.br>
2026-09-10 19:39:28 +0000

    lição 3 pronta

---
+++ binario_tech/aula16/auditoria_servidor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "==================--=================="
+echo " AUDITORIA DE SERVIDOR - BINÁRIO TECH"
+echo "====================================="
+
+echo "Listando os processos de node.js ativos no servidor e enviando para o arquivo..."
+ps aux | grep node >> .processos.log
+
+echo "Veja o Resultado da Lista:"
+sleep 2
+cat processos.log 
