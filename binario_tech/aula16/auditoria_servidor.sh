5a127f7bed0094f22f3fbdd7ef70b5aae57ac82e
Maely Peixoto Santos <maely.santos@edu.senai.br>
2026-09-10 23:39:08 +0000

    adiciona script auditoria_servidor.sh

--- binario_tech/aula16/auditoria_servidor.sh
+++ binario_tech/aula16/auditoria_servidor.sh
@@ -4,7 +4,7 @@ echo " AUDITORIA DE SERVIDOR - BINÁRIO TECH"
 echo "====================================="
 
 echo "Listando os processos de node.js ativos no servidor e enviando para o arquivo..."
-ps aux | grep node >> .processos.log
+ps aux | grep node >> processos.log
 
 echo "Veja o Resultado da Lista:"
 sleep 2
